986f49e09e848b7628cc093eee8e903bb44879e3
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-27 16:40:12 -0300

    Set UI Main Page

--- src/Pages/Home/Main/index.js
+++ src/Pages/Home/Main/index.js
@@ -8,7 +8,43 @@ export default function Main(){
     return(
         <div className='main-container'>
             <Carrosel/>
-            <section className='mais-vendidos'>
+            <h2>Mais Vendidos</h2>
+            <section className='mais-vendidos'>         
+                <article>
+                    <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+                    <div>
+                        <span className='produto'>Nome do Produto</span>
+                        <span className='preco'>R$100<span>R$200</span></span>
+                    </div>
+                </article>
+                <article>
+                    <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+                    <div>
+                        <span className='produto'>Nome do Produto</span>
+                        <span className='preco'>R$100<span>R$200</span></span>
+                    </div>
+                </article>
+                <article>
+                    <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+                    <div>
+                        <span className='produto'>Nome do Produto</span>
+                        <span className='preco'>R$100<span>R$200</span></span>
+                    </div>
+                </article>
+                <article>
+                    <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+                    <div>
+                        <span className='produto'>Nome do Produto</span>
+                        <span className='preco'>R$100<span>R$200</span></span>
+                    </div>
+                </article>
+                <article>
+                    <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+                    <div>
+                        <span className='produto'>Nome do Produto</span>
+                        <span className='preco'>R$100<span>R$200</span></span>
+                    </div>
+                </article>    
             </section>
             <section className='categorias'></section>
         </div>
