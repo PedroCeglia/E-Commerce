fb25ef3392b3a9067fdf9a175cae51e2cdb3e13c
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-29 19:03:28 -0300

    Finish UI ItemCarrinho

--- src/Pages/Home/Carrinho/ItemCarrinho/index.js
+++ src/Pages/Home/Carrinho/ItemCarrinho/index.js
@@ -3,6 +3,24 @@ import './style.css'
 
 export default function ItemCarrinho(){
     return(
-        <div className='item-carrinho-container'></div>
+        <tr>
+            <td className='td-item'>
+                <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+                <span>Nome do Produto</span>
+            </td>
+            <td className='td-preco'>
+                <span>R$100</span>
+            </td>
+            <td className='td-quantidade'>
+                <span>2</span>
+            </td>
+            <td className='td-subtotal'>
+                <span>R$200</span>
+            </td>
+            <td className='td-remove-edit'>
+                <img src='../assets/pencil.png' alt='Edit Icon'/>
+                <img src='../assets/close.png' alt='Remove Icon'/>
+            </td>
+        </tr>
     )
 }
