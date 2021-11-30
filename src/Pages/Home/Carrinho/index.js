9512282fc942aa67987d678fab26caa133bc4b21
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-30 19:02:15 -0300

    Finish Carrinho UI

--- src/Pages/Home/Carrinho/index.js
+++ src/Pages/Home/Carrinho/index.js
@@ -1,8 +1,36 @@
 import React from 'react'
 import './style.css'
 
+// Import Widgets
+import ItemCarrinho from './ItemCarrinho'
+
 export default function Carrinho(){
     return(
-        <div className='carrinho-container'></div>
+        <div className='carrinho-container'>
+            <h2>Carrinho de Compras</h2>
+            <div className='carrinho-content'>
+                <table className='itens-carrinho-content'>
+                    <thead>
+                        <tr>
+                            <th>Item</th>
+                            <th>Preço</th>
+                            <th>Quantidade</th>
+                            <th>Subtotal</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <ItemCarrinho/>
+                        <ItemCarrinho/>                        
+                    </tbody>
+                </table>
+                <div className='valor-carrinho-content'>
+                    <div><span>Subtotal</span><span className='valor'>R$100</span></div>
+                    <div><span>Total do Pedido</span><span className='valor'>R$100</span></div>
+                    <button>Fechar Pedido</button>
+                </div>
+            </div>
+            <div className='carrinho-buttons-area'></div>
+        </div>
     )
 }
