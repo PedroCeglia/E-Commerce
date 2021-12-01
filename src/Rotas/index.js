0e9680347362df15903bd0e82235916d5357d5d6
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-01 16:45:30 -0300

    Add Adm Page Route

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -3,13 +3,18 @@ import React from 'react'
 // Import React Router Dom
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-// Import Widgets
+// Import Home Pages
 import Home from '../Pages/Home'
 import Main from '../Pages/Home/Main'
-import Erro from '../Pages/Erro'
 import Entrar from '../Pages/Home/Entrar'
 import Produto from '../Pages/Home/Produto'
 import Carrinho from '../Pages/Home/Carrinho'
+
+// Import Erro Page
+import Erro from '../Pages/Erro'
+
+// Import ADM Pages
+import Adm from '../Pages/Adm'
 
 export default function Rotas(){
     return(
@@ -21,7 +26,7 @@ export default function Rotas(){
                     <Route path='/produto' element={<Produto/>}/>
                     <Route path='/carrinho' element={<Carrinho/>}/>
                 </Route>
-                <Route path='/adm' element={<></>}></Route>
+                <Route path='/adm' element={<Adm/>}></Route>
                 <Route path='*' element={<Erro/>} />
                 
             </Routes>
