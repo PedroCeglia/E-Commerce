ee92c74659b3e86074468bc21f1d09c1b045eb2e
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-01 09:22:53 -0300

    Add listenerUserAuth Function

--- src/Firebase/API/AuthApi/index.js
+++ src/Firebase/API/AuthApi/index.js
@@ -4,3 +4,7 @@ import { auth } from "../../FirebaseConfig";
 // Import Auth API´s
 import { onAuthStateChanged } from "@firebase/auth";
 
+// Listener User Auth
+function listenerUserAuth(setUser){
+    onAuthStateChanged(auth, user =>{setUser(user)})
+}
