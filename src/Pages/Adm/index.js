b2454deea7378f1c565fc382113d2ae0575da9e9
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-01 16:44:56 -0300

    Add Adm Page

---
+++ src/Pages/Adm/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+export default function Adm(){
+    return(
+        <div className='adm-container'>
+            <div className='carrosel-adm-container'></div>
+            <div className='mais-vendidos-adm-container'></div>
+            <div className='produtos-adm-container'></div>
+        </div>
+    )
+}
