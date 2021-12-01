b870bc67131362c7cc407c963b4bbbbe22bc1fc9
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-01 10:27:31 -0300

    Export ListenerUserAuth

---
+++ src/Firebase/API/DatabaseApi/index.js
@@ -0,0 +1,14 @@
+// Import Database Reference
+import { database } from "../../FirebaseConfig";
+
+// Import Database API´s
+import { ref, onValue } from "@firebase/database";
+
+// Get User By Id Listener
+export function getUserByIdListener(id, setUser){
+    onValue(ref(database,`usuarios/${id}`), snapshot =>{
+        if(snapshot.exists){
+            setUser(snapshot.val()) 
+        }
+    })
+}
