15f5f3351aca19e7552a06a6b62d53e180f69dd7
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-01 10:22:13 -0300

    Get User In Firebase And SetUser In Store Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
+
+// Import React Redux
+import { connect } from 'react-redux';
 
 // Import Rotas
 import Rotas from './Rotas';
 
-function App() {
+// Import AuthAPI
+import {listenerUserAuth} from './Firebase/API/AuthApi'
+
+// Import DatabaseAPI
+import {getUserByIdListener} from './Firebase/API/DatabaseApi'
+
+function App({dispatch}) {
+
+  // Get User Auth
+  const [userAuth, setUserAuth] = useState()
+  useEffect(()=>{
+    listenerUserAuth(setUserAuth)
+  },[])
+
+  // Get User Database
+  const [userDatabase, setUserDatabase] = useState()
+  useEffect(()=>{
+    if(userAuth != null){
+      getUserByIdListener(userAuth.uid, setUserDatabase)
+    }
+  },[userAuth])
+
+  // Edit User In Redux Store
+  const setUserInReduxStore = useCallback( exist => {
+    if(exist !== false){
+      return {
+        type:'SET_USER',
+        user:{
+          exist:true,
+          user:userDatabase
+        }
+      }      
+    } else {
+      return {
+        type:'SET_USER',
+        user:{
+          exist:false,
+          user:{}
+        }
+      }
+    }
+  },[userDatabase])
+  useEffect(()=>{
+    if(userDatabase != null){
+      dispatch(setUserInReduxStore(true))
+    } else{
+      dispatch(setUserInReduxStore(false))
+    }
+  },[userDatabase, dispatch, setUserInReduxStore])
+
+
+
   return (
     <Rotas/>
   );
 }
 
-export default App;
+export default connect()(App);
