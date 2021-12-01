c20b3c2d60952f12948ba7a4bb5ea63f2ffa8763
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-01 10:20:01 -0300

    Corrigindo Bug Na UI

--- src/Pages/Erro/index.js
+++ src/Pages/Erro/index.js
@@ -4,7 +4,7 @@ import './style.css'
 
 export default function Erro(){
     return(
-        <div class='erro-container'>
+        <div className='erro-container'>
             <div className='erro-content'>
                 <h2>Erro 404</h2>
                 <span>Page Not Found</span>
