34632b3c0f43dfd9bf6ac00457d1094345cdc9f8
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-01 10:20:45 -0300

    Import and Use Provider and store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Import React Redux
+import {Provider} from 'react-redux'
+
+// Import Store
+import store from './Redux/Store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
