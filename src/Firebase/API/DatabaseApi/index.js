25dc008eb40fb4424a546efe38ccd72459aecd56
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-14 21:35:20 -0300

    Add createUserByDatabase, setUserNameInDatabase and addProdutoInDatabase

--- src/Firebase/API/DatabaseApi/index.js
+++ src/Firebase/API/DatabaseApi/index.js
@@ -2,7 +2,11 @@
 import { database } from "../../FirebaseConfig";
 
 // Import Database API´s
-import { ref, onValue } from "@firebase/database";
+import { ref, onValue, update, push } from "@firebase/database";
+
+
+////////////////////////////////////////-------FUNCTIONS--------//////////////////////////////////////
+
 
 // Get User By Id Listener
 export function getUserByIdListener(id, setUser){
@@ -10,6 +14,35 @@ export function getUserByIdListener(id, setUser){
         if(snapshot.exists){
             setUser(snapshot.val()) 
         }
+    })
+}
+
+// Add User In Database
+export function createUserByDatabase(email, userId){
+    update(ref(database,`usuarios/${userId}`),{
+        email:email,
+        id:userId
+    })
+} 
+
+// Set User Name In Database
+export function setUserNameInDatabase(name, userId){
+    update(ref(database,`usuarios/${userId}`),{
+        nome:name
+    })
+}
+
+// Add Produto In Database
+export function addProdutoInDatabase(nome, listaDeFotos, precoOg, precoPromo, promoPorc, descricao, listCaracteristica){
+    const produtoId = push(ref(database,`anuncios`)).key
+    update(ref(database,`anuncios/${produtoId}`),{
+        nomeProduto:nome,
+        precoOg:precoOg,
+        precoPromo:precoPromo,
+        promoPorc:promoPorc,
+        descricaoProduto:descricao,
+        caracteristicas:listCaracteristica,
+        fotos:listaDeFotos
     })
 }
  
