e1ee42fd1405249399d7da77ece01b551dadcaeb
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-16 20:47:23 -0300

    Add removeProdutoInDatabase, getProdutosInDatabase, getProdutoById, addProdutoInCarrosel, removeProdutoInCarrosel, addProdutoInMaisVendidos and removeProdutoInMaisVendidos Functions

--- src/Firebase/API/DatabaseApi/index.js
+++ src/Firebase/API/DatabaseApi/index.js
@@ -2,7 +2,7 @@
 import { database } from "../../FirebaseConfig";
 
 // Import Database API´s
-import { ref, onValue, update, push } from "@firebase/database";
+import { ref, onValue, update, push, remove } from "@firebase/database";
 
 
 ////////////////////////////////////////-------FUNCTIONS--------//////////////////////////////////////
@@ -34,15 +34,70 @@ export function setUserNameInDatabase(name, userId){
 
 // Add Produto In Database
 export function addProdutoInDatabase(nome, listaDeFotos, precoOg, precoPromo, promoPorc, descricao, listCaracteristica){
-    const produtoId = push(ref(database,`anuncios`)).key
-    update(ref(database,`anuncios/${produtoId}`),{
+    const produtoId = push(ref(database,`anuncios/produtos`)).key
+    update(ref(database,`anuncios/produtos/${produtoId}`),{
         nomeProduto:nome,
         precoOg:precoOg,
         precoPromo:precoPromo,
         promoPorc:promoPorc,
         descricaoProduto:descricao,
         caracteristicas:listCaracteristica,
-        fotos:listaDeFotos
+        fotos:listaDeFotos,
+        id:produtoId
     })
 }
- 
+
+// Remove Produto In Database
+export function removeProdutoInDatabase(id){
+    remove(ref(database,`anuncios/produtos/${id}`))
+}
+
+// Get Produtos In Database
+export function getProdutosInDatabase(setProdutosList){
+    onValue(ref(database,'anuncios/produtos'), snapshot => {
+        let listProduto = []
+        if(snapshot.exists()){
+            snapshot.forEach(produto => {
+                listProduto.push(produto.val())
+            })   
+        } 
+        setProdutosList(listProduto)
+    })
+}
+
+// Get Produto By Id
+export function getProdutoById(id, setProduto){
+    onValue(ref(database,`anuncios/produto/${id}`), snapshot => {
+        if(snapshot.exists()){
+            setProduto(snapshot.val())
+        }
+    })
+}
+
+// Add Produto In Carrosel 
+export function addProdutoInCarrosel(id, foto, nome){
+    update(ref(database,`anuncios/carrosel/${id}`),{
+        id:id,
+        foto:foto,
+        nome:nome
+    })
+}
+
+// Remove Produto In Carrosel
+export function removeProdutoInCarrosel(id){
+    remove(ref(database,`anuncios/carrosel/${id}`))
+}
+
+// Add Produto In Mais Vendidos 
+export function addProdutoInMaisVendidos(id, foto, nome){
+    update(ref(database,`anuncios/maisVendidos/${id}`),{
+        id:id,
+        foto:foto,
+        nome:nome
+    })
+}
+
+// Remove Produto In Mais Vendidos
+export function removeProdutoInMaisVendidos(id){
+    remove(ref(database,`anuncios/maisVendidos/${id}`))
+}
