264698fdaabd6a7ef9a7b032f3e4f1ba9db2873c
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-16 20:44:18 -0300

    Add Produtos Reducer

--- src/Store/Duck/index.js
+++ src/Store/Duck/index.js
@@ -1,11 +1,18 @@
 // Import Redux
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 // Import User
 import user from "./user";
+import produtos from "./produtos";
+
+// CombineReducers
+const reducer = combineReducers({
+    user:user,
+    produtos:produtos
+})
 
 // Create Store
-const store = createStore(user) 
+const store = createStore(reducer) 
 
 // Export Store to src/index.js
 export default store
