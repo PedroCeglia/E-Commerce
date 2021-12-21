02746141f072695f8b18a449e2c94cffb2b9fc0f
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-12-21 17:58:18 -0300

    Finish Produto Carrosel Widget

--- src/Pages/Home/Produto/Carrosel/index.js
+++ src/Pages/Home/Produto/Carrosel/index.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './style.css'
 
-export default function Carrosel(){
+export default function Carrosel(props){
+
+    const [fotos, setFotos] = useState([])
+    const [fotoMain, setFotoMain] = useState('../assets/fone-de-ouvido-jbl.jpeg')
+    useEffect(()=>{
+      if(props.fotos[0] != null ){
+        setFotos(props.fotos)
+        setFotoMain(props.fotos[0].url)
+      }else{
+        setFotos([])
+        setFotoMain('../assets/fone-de-ouvido-jbl.jpeg')
+      }
+    },[props.fotos])
 
     return (
       <div className='carrosel-produto-container'>
         <div className='choice-image'>
-          <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
-          <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
-          <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
-          <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+          {fotos.map((item, key) => {
+            return(
+              <img src={item.url} key={key} onClick={()=>{setFotoMain(item.url)}} alt='Produto'/>
+            )
+          })}
         </div>
         <div className='image-content'>
-          <img src='../assets/fone-de-ouvido-jbl.jpeg' alt='Produto'/>
+          <img src={fotoMain} alt='Produto'/>
         </div>
       </div>
     )
